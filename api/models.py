44f688a274171c367144426d4b3f1bb221227b89
SNeXT <snext2004@gmail.com>
2025-12-24 21:36:53 +0700

    first endpoints

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+class Choice(models.Model):
+    id = models.AutoField(primary_key=True)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # Relation
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # question_id
--- my_django/settings.py
+++ my_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",") if not DEBUG else []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
