9eb01c33244014347ec5d6805f9b7d49b978d139
SNeXT <snext2004@gmail.com>
2025-12-24 22:16:20 +0700

    try first request.data

--- api/views.py
+++ api/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class HelloWorld(APIView):
     def get(self, request):
+        if not ("name" in request.data and "age" in request.data):
+            return Response(
+                {
+                    "msg": "Bad Request!!!"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        name = request.data["name"]
+        age = request.data["age"]
         return Response(
-            "Hello Worlds"
+            {
+                "msg": "Hello",
+                "name": name,
+                "age": age
+            }
         )
